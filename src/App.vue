901b92351b082186dcebff5e125044e82ff8c9f0
beginnerZzt <2521946696zzt@gmail.com>
2023-04-11 12:01:53 +0800

    feat(非路由组件): 展示非路由组件

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-
+    <Header></Header>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
+import Header from "@/components/Header/indexHeader";
+import Footer from "@/components/Footer/indexFooter";
 export default {
-
+  components: {
+    Header,
+    Footer,
+  }
 }
 </script>
 
